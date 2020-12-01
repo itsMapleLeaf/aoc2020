7557f0f7ee35b87e7641c58c2cfee5e45671214f
kingdaro <19603573+kingdaro@users.noreply.github.com>
2020-12-01 01:28:17 -0600

    this level of DRY is completely unnecessary

--- src/main/kotlin/day01/main.kt
+++ src/main/kotlin/day01/main.kt
@@ -6,9 +6,10 @@ import java.io.File
 
 fun main() {
 	val input = File("day01.input.txt").readLines().map { it.toInt() }
-	val answer1 = input.combinations(2).find { it.sum() == 2020 }?.product()
-	val answer2 = input.combinations(3).find { it.sum() == 2020 }?.product()
 
-	println("answer 1: $answer1")
-	println("answer 2: $answer2")
+	fun find2020SumProduct(combinationSize: Int) =
+		input.combinations(combinationSize).find { it.sum() == 2020 }?.product()
+
+	println("answer 1: ${find2020SumProduct(2)}")
+	println("answer 2: ${find2020SumProduct(3)}")
 }
