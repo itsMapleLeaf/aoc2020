31f2f351b5735d278b7489cc8a74759461f8315d
kingdaro <19603573+kingdaro@users.noreply.github.com>
2020-12-04 01:03:19 -0600

    day 4 more cleanup

--- src/main/kotlin/day04/main.kt
+++ src/main/kotlin/day04/main.kt
@@ -3,39 +3,22 @@ package day04
 import java.io.File
 
 typealias Passport = Map<String, String>
+typealias ValidateFn = (String) -> Boolean
 
-fun passportFromString(passportString: String): Passport =
-    passportString
-        .split(Regex("\\s+"))
-        .map(::pairFromFieldString)
-        .toMap()
-
-fun pairFromFieldString(fieldString: String): Pair<String, String> {
-    val (name, value) = fieldString.split(":")
-    return Pair(name, value)
-}
+data class Validation(val name: String, val value: String?, val isValid: Boolean)
 
 val requiredFields = mapOf(
-    "byr" to { it.length == 4 && it.toIntOrNull() in 1920..2002 },
-    "iyr" to { it.length == 4 && it.toIntOrNull() in 2010..2020 },
-    "eyr" to { it.length == 4 && it.toIntOrNull() in 2020..2030 },
+    "byr" to validateYear(1920..2002),
+    "iyr" to validateYear(2010..2020),
+    "eyr" to validateYear(2020..2030),
     "hgt" to ::validateHeight,
     "hcl" to { Regex("^#[0-9a-f]{6}$").matches(it) },
     "ecl" to { it in setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth") },
     "pid" to { Regex("^\\d{9}$").matches(it) },
 )
 
-fun Passport.hasRequiredFields() =
-    keys.containsAll(requiredFields.keys)
-
-fun Passport.isValid() = requiredFields.all { (fieldName, getIsValid) ->
-    val value = this[fieldName] ?: return@all false
-    val isValid = getIsValid(value)
-    if (!isValid) {
-        println("invalid field $fieldName:$value")
-    }
-    isValid
-}
+fun validateYear(range: IntRange): ValidateFn =
+    { it.length == 4 && it.toIntOrNull() in range }
 
 fun validateHeight(heightValue: String): Boolean {
     val result = Regex("^(\\d+)(cm|in)$").find(heightValue) ?: return false
@@ -51,6 +34,30 @@ fun validateHeight(heightValue: String): Boolean {
 
     return value in range
 }
+
+fun passportFromString(passportString: String): Passport =
+    passportString
+        .split(Regex("\\s+"))
+        .map(::pairFromFieldString)
+        .toMap()
+
+fun pairFromFieldString(fieldString: String): Pair<String, String> {
+    val (name, value) = fieldString.split(":")
+    return Pair(name, value)
+}
+
+fun Passport.hasRequiredFields() =
+    keys.containsAll(requiredFields.keys)
+
+fun Passport.isValid() = requiredFields
+    .map { (fieldName, validateFn) ->
+        val value = get(fieldName)
+        val isValid = value != null && validateFn(value)
+        Validation(fieldName, value, isValid)
+    }
+    .onEach { if (!it.isValid) println("invalid field ${it.name}: ${it.value ?: "<missing>"}") }
+    .all { it.isValid }
+
 
 fun main() {
     val passports: List<Passport> = File("day04.input.txt").readText()
